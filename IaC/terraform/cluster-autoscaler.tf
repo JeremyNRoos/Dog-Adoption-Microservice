1b102f8bb01bdec70e26dfda47209a67d8a5a008
Jeremy Roos <jeremy.n.roos@gmail.com>
2025-11-26 02:34:13 -0500

    got autoscaler to work, and pushed db-secret (didnt push before for some reason)

--- IaC/terraform/cluster-autoscaler.tf
+++ IaC/terraform/cluster-autoscaler.tf
@@ -65,28 +65,28 @@ resource "aws_eks_pod_identity_association" "cluster_autoscaler" {
 }
 
 resource "helm_release" "cluster_autoscaler" {
-  name = "autoscaler"
-
+  name       = "autoscaler"
+  namespace  = "kube-system"
   repository = "https://kubernetes.github.io/autoscaler"
   chart      = "cluster-autoscaler"
-  namespace  = "kube-system"
   version    = "9.37.0"
 
-  set {
-    name  = "rbac.serviceAccount.name"
-    value = "cluster-autoscaler"
-  }
+  set = [
+    {
+      name  = "rbac.serviceAccount.name"
+      value = "cluster-autoscaler"
+    },
+    {
+      name  = "autoDiscovery.clusterName"
+      value = aws_eks_cluster.eks.name
+    },
+    {
+      name  = "awsRegion"
+      value = "us-east-1"
+    }
+  ]
 
-  set {
-    name  = "autoDiscovery.clusterName"
-    value = aws_eks_cluster.eks.name
-  }
-
-  # MUST be updated to match your region 
-  set {
-    name  = "awsRegion"
-    value = "us-east-2"
-  }
-
-  depends_on = [helm_release.metrics_server]
+  depends_on = [
+    helm_release.metrics_server,
+  ]
 }
